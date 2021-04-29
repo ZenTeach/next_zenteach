b03b853fa2432aca65722e7c798c0eace56e193d
Natnael Getahun <connect@ngetahun.me>
2021-04-29 22:36:44 +0200

    Add privacy policy & terms and conditions

---
+++ pages/privacy_policy.js
@@ -0,0 +1,155 @@
+import SiteLayout from '../components/SiteLayout'
+
+const PrivacyPolicy = () => {
+	return (
+<SiteLayout>
+			<div className="h-full bg-grey mt-8 sm:mt-0 sm:py-12 max-w-4xl mx-auto px-8">
+				<h1 className="text-5xl text-center font-bold my-2">Privacy Policy</h1>
+				<small className="text-xs italic my-2">Last updated: July 9, 2020</small>
+				<p>
+					ZenTeach (“us”, “we”, or “our”) operates the ZenTeach website (the “Service”).
+
+					This page informs you of our policies regarding the collection, use and disclosure
+					of Personal Information when you use our Service.
+
+					We will not use or share your information with anyone except as described in this
+					Privacy Policy.
+
+					We use your Personal Information for providing and improving the Service. By using
+					the Service, you agree to the collection and use of information in accordance with
+					this policy. Unless otherwise defined in this Privacy Policy, terms used in this
+					Privacy Policy have the same meanings as in our Terms and Conditions, accessible at https://zenteach.co.uk
+				</p>
+				<h3 className="text-3xl font-bold">Information Collection And Uses</h3>
+				<h5 className="text-xl font-bold">Teacher member.</h5>
+				<p className="my-2">
+					While using our Service, we may ask you to provide us with certain personally identifiable
+					information that can be used to contact or identify you. Personally identifiable information
+					(“Personal Information”) may include, but is not limited to:
+					<ul className="mx-3 list-disc my-2">
+						<li>Your name (compulsory)</li>
+						<li>Your email address (compulsory)</li>
+						<li>Your school name (compulsory)</li>
+						<li>Students’ names (compulsory)</li>
+						<li>Students’ email addresses (compulsory)</li>
+						<li>Students’ classes (compulsory)</li>
+						<li>Subgroup data such as SEN, FSM, PP etc… (not compulsory)</li>
+					</ul>
+					It is the responsibility of each Customer being the educational establishment or an individual
+					teacher to ensure that any student to whom they intend provide with a login (including, without
+					limitation, students under the age of 13) have, before any such login is provided, obtained all
+					necessary consents and permissions (from a parent and/or guardian) to use the Platform and for
+					us to collect and process any user data.
+
+					You are also reminded that upon a student attaining the age of 13 there is a legal requirement
+					to retake consent directly from the student and we require the relevant School to secure that consent.
+
+					As a Teacher user you acknowledge and warrant that you have and will provide all requisite authority
+					from, and on behalf of, the student including where relevant the parent/guardian of the student and
+					on behalf of the educational establishment to (i) for you to provide the Platform and make it available
+					to student users; (ii) allow the student to use the Platform and (iii) and to make any User Data available
+					to us in connection with the use of the Platform on the terms set out in our terms and conditions and
+					this privacy policy.
+				</p>
+				<h5 className="text-xl font-bold">What is the lawful basis for storing this information?</h5>
+				<p className="my-2">
+					This information is needed for teachers and students to be able to use the ZenTeach
+					platform for online testing and revision, which is the core of ZenTeach’s service.
+				</p>
+
+				<h3 className="text-3xl font-bold">Log Data</h3>
+				<p className="my-2">
+					We collect information that your browser sends whenever you visit our
+					Service (“Log Data”). This Log Data may include information such as your computer’s
+					Internet Protocol (“IP”) address, browser type, browser version, the pages of our
+					Service that you visit, the time and date of your visit, the time spent on those
+					pages and other statistics.
+
+					The information is stored on ZenTeach’s servers in data centres in Ireland,
+					provided by Amazon Web Services (AWS). AWS data centres are compliant with the
+					international information security standard, ISO 27001.
+
+					For more information about AWS’s ISO 27001 certification, please visit this webpage:
+					<a className="text-blue" href="https://aws.amazon.com/compliance/iso-27001-faqs/">
+						https://aws.amazon.com/compliance/iso-27001-faqs/
+					</a>
+
+					For more information about AWS security, please visit this webpage:
+					<a className="https://aws.amazon.com/security/">
+						https://aws.amazon.com/security/
+					</a>
+				</p>
+				<h3 className="text-3xl font-bold">Cookies</h3>
+				<p className="my-2">
+					Cookies are files with small amount of data, which may include an
+					anonymous unique identifier. Cookies are sent to your browser from a web site
+					and stored on your computer’s hard drive.
+
+					We use “cookies” to collect information. You can instruct your browser to refuse
+					all cookies or to indicate when a cookie is being sent. However, if you do not
+					accept cookies, you may not be able to use some portions of our Service.
+				</p>
+
+				<h3 className="text-3xl font-bold">Service Providers</h3>
+				<p className="my-2">
+					We may employ third party companies and individuals to facilitate our
+					Service, to provide the Service on our behalf, to perform
+					Service-related services or to assist us in analyzing how our Service is used.
+
+					These third parties have access to your Personal Information only to
+					perform these tasks on our behalf and are obligated not to disclose or use
+					it for any other purpose.
+				</p>
+
+				<h3 className="text-3xl font-bold">Security</h3>
+				<p className="my-2">
+					The security of your Personal Information is important to us, but remember that no
+					method of transmission over the Internet, or method of electronic storage is
+					100% secure. While we strive to use commercially acceptable means to protect
+					your Personal Information, we cannot guarantee its absolute security.
+				</p>
+
+				<h3 className="text-3xl font-bold">Links To Other Sites</h3>
+				<p className="my-2">
+					Our Service may contain links to other sites that are not operated by us. If you click
+					on a third party link, you will be directed to that third party’s site. We strongly
+					advise you to review the Privacy Policy of every site you visit.
+
+					We have no control over, and assume no responsibility for the content, privacy
+					policies or practices of any third party sites or services.
+				</p>
+
+				<h3 className="text-3xl font-bold">Children’s Privacy</h3>
+				<p className="my-2">
+					Our Service does not address anyone under the age of 18 (“Children”).
+
+					We do not knowingly collect personally identifiable information from children
+					under 18. If you are a parent or guardian and you are aware that your child
+					has provided us with Personal Information, please contact us. If we discover
+					that a child under 18 has provided us with Personal Information, we will delete such information from our servers immediately.
+				</p>
+
+				<h3 className="text-3xl font-bold">Compliance With Laws</h3>
+				<p className="my-2">
+					We will disclose your Personal Information where required to do so by law
+					or subpoena.
+				</p>
+				<h3 className="text-3xl font-bold">Changes To This Privacy Policy</h3>
+				<p className="my-2">
+					We may update our Privacy Policy from time to time. We will notify you of
+					any changes by posting the new Privacy Policy on this page.
+
+					You are advised to review this Privacy Policy periodically for any
+					changes. Changes to this Privacy Policy are effective when they are posted on this page.
+				</p>
+
+				<h3 className="text-3xl font-bold">Contact Us</h3>
+				<p className="my-2b">
+					If you have any questions about these Terms, please contact us (info@zenteach.co.uk).
+				</p>
+			</div>
+		</SiteLayout>
+	);
+}
+
+export default PrivacyPolicy
---
+++ pages/terms_and_conditions.js
@@ -0,0 +1,148 @@
+import SiteLayout from '../components/SiteLayout'
+
+const TermsAndConditions = () => {
+	return (
+		<SiteLayout>
+			<div className="h-full bg-grey mt-8 sm:mt-0 sm:py-12 max-w-4xl mx-auto px-8">
+				<h1 className="text-5xl text-center font-bold my-2">Terms and Conditions</h1>
+				<small className="text-xs italic my-2">Last updated: July 9, 2020</small>
+				<p>
+					Please read these Terms of Use (“Terms”, “Terms of Use”) carefully
+					before using the https://zenteach.co.uk website (the “Service”)
+					operated by ZenTeach (“us”, “we”, or “our”).
+
+					Your access to and use of the Service is conditioned on your acceptance
+					of and compliance with these Terms. These Terms apply to all visitors,
+					users and others who access or use the Service.
+
+					By accessing or using the Service you agree to be bound by these Terms.
+					If you disagree with any part of the terms then you may not access the Service.
+				</p>
+				<h3 className="text-3xl font-bold">Accounts</h3>
+				<p className="my-2">
+					When you create an account with us, you must provide us information that is accurate,
+					complete, and current at all times. Failure to do so constitutes a breach of the Terms,
+					which may result in immediate termination of your account on our Service.
+
+					You are responsible for safeguarding the password that you use to access the Service
+					and for any activities or actions under your password, whether your password is with our
+					Service or a third-party service.
+
+					You agree not to disclose your password to any third party. You must notify us immediately
+					upon becoming aware of any breach of security or unauthorized use of your account.
+				</p>
+				<h3 className="text-3xl font-bold">Intellectual Property</h3>
+				<p className="my-2">
+					The Service and its original content, features and functionality are and will remain the
+					exclusive property of ZenTeach and its licensors.
+				</p>
+				<h3 className="text-3xl font-bold">User-Uploaded Content</h3>
+				<p className="my-2">
+					Our website allows you to upload content (“User-Uploaded Content”) for the exchange of lawful,
+					relevant, fair and appropriate information, opinions, materials and comments with other users.
+					You acknowledge that all User-Uploaded Content is the sole responsibility of the person from
+					which such content originated. We accept no responsibility for, and do not guarantee, the accuracy,
+					integrity or quality of any User-Uploaded Content.
+
+					Please be aware that any user who is aggrieved by your User-Uploaded Content may take legal action
+					against you. This could result in data concerning your identity being disclosed and you having to pay
+					compensation to the aggrieved party.
+
+					We do not pre-screen User-Uploaded Content but we have the right to monitor User-Uploaded Content if we
+					wish. If we determine that any User-Uploaded Content is inappropriate or otherwise objectionable, we may
+					remove any User-Uploaded Content from the Websites without any liability to you or to a third party.
+				</p>
+				<h3 className="text-3xl font-bold">Use of content you upload</h3>
+				<p className="my-2">
+					When you upload User-Uploaded Content to our Websites, you will (assuming you are the owner of the content)
+					retain all intellectual property rights in such content. This means that you will be able to modify or remove
+					your User-Uploaded Content as you wish. To enable us to provide our Services and host your User-Uploaded Content,
+					we require a licence. Similarly, third party users of our Websites will require a licence from you to enable
+					them to make use of your User-Uploaded Content.
+
+					The licence you grant us and each of our affiliates is non-exclusive, sub-licensable, worldwide, fully paid-up,
+					royalty-free, perpetual and irrevocable (save as set out below). This means that we can use, reproduce, publish,
+					distribute and display your User-Uploaded Content (in whole or in part) and/or incorporate it in other works in any
+					form, media, or technology. Third party users are allowed to download and/or otherwise use your User-Uploaded
+					Content under an Attribution License:
+					<ul>
+						<li><span className="font-bold">Share</span> — copy and redistribute the material in any medium or format</li>
+						<li><span className="font-bold">Adapt</span> — remix, transform, and build upon the material for any purpose, even commercially.</li>
+					</ul>
+				</p>
+				<h3 className="text-3xl font-bold">Your use of other peoples’ content</h3>
+				<p className="my-2">
+					Services that we provide, allow you to use third party users’ User-Uploaded Content within our Websites.
+					You acknowledge that your use of such User-Uploaded Content does not entitle you to any intellectual property
+					rights to such User-Uploaded Content. All User-Uploaded Content will be governed by the relevant “upload” licence applied
+					by the person uploading the content (third party user).
+				</p>
+				<h3 className="text-3xl font-bold">Objections to User-Uploaded Content</h3>
+				<p className="my-2">
+					You acknowledge that by using our Websites, there is a risk that you may be exposed to User-Uploaded Content that you may
+					find offensive, indecent or objectionable. If you object to the publication of any User-Uploaded Content made available through
+					our Websites, please contact us via email with a URL to the content.
+				</p>
+				<h3 className="text-3xl font-bold">Links To Other Web Sites</h3>
+				<p className="my-2">
+					Our Service may contain links to third-party web sites or services that are not owned or controlled by ZenTeach.
+
+					ZenTeach has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any
+					third party web sites or services. You further acknowledge and agree that ZenTeach shall not be responsible or liable,
+					directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance
+					on any such content, goods or services available on or through any such web sites or services.
+
+					We strongly advise you to read the terms and conditions and privacy policies of any third-party web sites or services that you visit.
+				</p>
+				<h3 className="text-3xl font-bold">Termination</h3>
+				<p className="my-2">
+					We may terminate or suspend access to our Service immediately, without prior notice or liability, for any reason whatsoever,
+					including without limitation if you breach the Terms.
+
+					All provisions of the Terms which by their nature should survive termination shall survive termination, including,
+					without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability.
+
+					We may terminate or suspend your account immediately, without prior notice or liability, for any reason whatsoever,
+					including without limitation if you breach the Terms.
+
+					Upon termination, your right to use the Service will immediately cease. If you wish to terminate your account,
+					you may simply discontinue using the Service.
+
+					All provisions of the Terms which by their nature should survive termination shall survive termination, including,
+					without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability.
+				</p>
+				<h3 className="text-3xl font-bold">Disclaimer</h3>
+				<p className="my-2">
+					Your use of the Service is at your sole risk. The Service is provided on an “AS IS” and “AS AVAILABLE” basis.
+					The Service is provided without warranties of any kind, whether express or implied, including, but not limited to,
+					implied warranties of merchantability, fitness for a particular purpose, non-infringement or course of performance.
+				</p>
+				<h3 className="text-3xl font-bold">Governing Law</h3>
+				<p className="my-2">
+					These Terms shall be governed and construed in accordance with the laws of United Kingdom without regard to its
+					conflict of law provisions.
+					Our failure to enforce any right or provision of these Terms will not be considered a waiver of those rights.
+					If any provision of these Terms is held to be invalid or unenforceable by a court, the remaining provisions of these
+					Terms will remain in effect. These Terms constitute the entire agreement between us regarding our Service, and supersede
+					and replace any prior agreements we might have between us regarding the Service.
+				</p>
+				<h3 className="text-3xl font-bold">Changes</h3>
+				<p className="my-2">
+					We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material
+					we will try to provide at least 30 days notice prior to any new terms taking effect. What constitutes a material change
+					will be determined at our sole discretion.
+
+					By continuing to access or use our Service after those revisions become effective, you agree to be bound by the revised terms.
+					If you do not agree to the new terms, please stop using the Service.
+				</p>
+
+				<h3 className="text-3xl font-bold">Contact Us</h3>
+				<p className="my-2b">
+					If you have any questions about these Terms, please contact us (info@zenteach.co.uk).
+				</p>
+			</div>
+		</SiteLayout>
+	);
+}
+
+export default TermsAndConditions
