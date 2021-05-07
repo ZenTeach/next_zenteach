a5adb7b3bbbbe12de9fe6cb33034b07e3546c12f
Natnael Getahun <connect@ngetahun.me>
2021-05-07 17:36:26 +0200

    Add fontawesome package
    Add about us page with social links
    
    Add pics for nate & philipp

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import App from 'next/app'
 import '../css/tailwind.css'
+import { config, library } from '@fortawesome/fontawesome-svg-core'
+import { fab,  faLinkedin, faTwitter, faGithub, faMedium } from '@fortawesome/free-brands-svg-icons'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+config.autoAddCss = false
+library.add(fab, faLinkedin, faTwitter, faGithub, faMedium)
 
 class MyApp extends App {
   render() {
---
+++ pages/about_us.js
@@ -0,0 +1,77 @@
+import SiteLayout from '../components/SiteLayout'
+import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const AboutUs = () => {
+	return (
+		<SiteLayout>
+			<div className="flex flex-row">
+				<div className="w-full bg-gradient-to-r from-rose-50 to-rose-100">
+					<div className="text-5xl text-center">Meet the Team</div>
+				</div>
+			</div>
+			<div className="w-full flex justify-center m-5 p-8 flex">
+				<div className="max-w-sm w-auto h-1/5 mx-4 my-4 flex flex-col justify-center overflow-hidden">
+					<div className="self-center">
+						<img className="object-cover w-32 h-auto object-center rounded-full p-2 justify-center"
+						src="philipp.jpg" alt="Philipp" />
+					</div>
+					<div className="px-6 py-4">
+						<div className="font-bold text-xl mb-2">Philipp Herzberg</div>
+						<span className="font-thin text-md mb-2">Business & Marketing</span>
+					</div>
+					<div className="px-6 pt-4 pb-2 w-full flex">
+						<div className="p-2 content-around space-around">
+							{/* <Link href=""></Link> */}
+								<a href="https://twitter.com/mikioherzberg" target="_blank">
+									<FontAwesomeIcon icon={["fab", "twitter"]} />
+								</a>
+						</div>
+						<div className="p-2 content-around space-around">
+							{/* <Link href=""></Link> */}
+								<a href="https://www.linkedin.com/in/philippherzberg/" target="_blank">
+									<FontAwesomeIcon icon={["fab", "linkedin"]} />
+								</a>
+						</div>
+					</div>
+				</div>
+				<div className="max-w-sm w-auto h-1/5 mx-4 my-4 flex flex-col justify-center overflow-hidden">
+					<div className="self-center">
+						<img
+							className="object-cover w-32 h-auto object-center rounded-full p-2 justify-center"
+							src="nate.png"
+							alt="Natnael" />
+					</div>
+
+					<div className="px-6 py-4">
+						<div className="font-bold text-xl mb-2">Natnael Getahun</div>
+						<span className="font-thin text-md mb-2">Tech & Product</span>
+					</div>
+					<div className="px-6 pt-4 pb-2 w-full flex">
+						<div className="p-2 content-around space-around">
+							{/* <Link href="https://twitter.com/babyodainaonsie"></Link> */}
+								<a href="https://twitter.com/babyodainaonsie" target="_blank">
+									<FontAwesomeIcon icon={["fab", "twitter"]} />
+								</a>
+						</div>
+						<div className="p-2 content-around space-around">
+							{/* <Link href="https://www.linkedin.com/in/ngetahun/"></Link> */}
+								<a href="https://www.linkedin.com/in/ngetahun/" target="_blank">
+									<FontAwesomeIcon icon={["fab", "linkedin"]} />
+								</a>
+						</div>
+						<div className="p-2 content-around space-around">
+							{/* <Link href="https://github.com/ngetahun"></Link> */}
+								<a href="https://github.com/ngetahun" target="_blank">
+									<FontAwesomeIcon icon={["fab", "github"]} />
+								</a>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</SiteLayout>
+	);
+}
+
+export default AboutUs
