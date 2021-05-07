76ef07e23ac945f5a4d4fb9868e9a93a4c714ab9
Natnael Getahun <connect@ngetahun.me>
2021-05-07 18:00:15 +0200

    Add zenteach social icons

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,53 @@
 import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Footer = ({}) => (
-	<div className="h-full bg-red border-black border-3 p-8">
-		<div className="sm:flex mx-auto mb-4 p-4">
-			<div className="sm:w-1/4 w-1/4 h-auto">
+	<div className="h-full bg-red flex justify-around p-8">
+		<div className="sm:flex mb-4 p-4">
+			<div className="sm:w-1/4 md:w-1/3 w-1/3 h-auto flex-shrink">
+				<div className="font-bold text-xl space-y-2 w-full mb-2">
+					<Link href="/">
+						<img className="h-8 w-8 float-left" src="/logo.png" alt="" />
+					</Link>
+					<span className="mx-2">ZenTeach</span>
+				</div>
+
+				<div className="my-6">
+					<p>Follow us on</p>
+					<div className="space-x-2 inline-flex flex-row justify-between w-1/2">
+						<span>
+							<a href="https://www.twitter.com/getzenteach" target="_blank">
+								<FontAwesomeIcon icon={["fab", "twitter"]}/>
+							</a>
+						</span>
+						<span>
+							<a href="https://www.linkedin.com/company/zenteach" target="_blank">
+								<FontAwesomeIcon icon={["fab", "linkedin"]}/>
+							</a>
+						</span>
+						<span>
+							<a href="https://github.com/ZenTeach" target="_blank">
+								<FontAwesomeIcon icon={["fab", "github"]}/>
+							</a>
+						</span>
+						<span>
+							<a href="https://zenteach.medium.com/" target="_blank">
+								<FontAwesomeIcon icon={["fab", "medium"]}/>
+							</a>
+						</span>
+					</div>
+				</div>
+			</div>
+			<div className="sm:w-1/4 md:w-1/3 w-1/3 h-auto">
 				<div className="font-bold text-xl mb-2">Company</div>
 				<ul className="list-reset leading-normal">
-					<li className="hover:text-orange text-grey-darker">About Us</li>
+					<li className="hover:text-orange text-grey-darker">
+						<Link href='/about_us'>
+							<a className="inline-block bg-white border-none text-red font-medium rounded-lg leading-tight">
+								About Us
+							</a>
+						</Link>
+					</li>
 					<li className="hover:text-orange text-grey-darker">
 						<Link href='/vision'>
 							<a className="inline-block bg-white border-none text-red font-medium rounded-lg leading-tight">
@@ -37,7 +78,7 @@ const Footer = ({}) => (
 					</li>
 				</ul>
 			</div>
-			<div className="sm:w-1/2 w-1/2 sm:mt-0 mt-8 h-auto">
+			<div className="sm:w-1/2 md:w-1/3 w-1/3 sm:mt-0 mt-8 h-auto">
 				<div className="text-2xl font-bold mb-2">Newsletter</div>
 				<p className="text-grey-darker leading-normal">Join our community and stay in touch with us!</p>
 				<div className="mt-4 flex">
