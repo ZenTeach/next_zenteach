539668f28435094df81fd6db03f263a2d2ae396b
Natnael Getahun <connect@ngetahun.me>
2021-05-07 21:18:15 +0200

    Remove unused links
    Add link to portal

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,22 +2,24 @@ import Link from "next/link";
 
 const NavBar = ({ children }) => (
 	<div>
-		<nav className="py-8 px-8 w-100 flex-shrink-0 flex justify-center">
-		<div>
+		<nav className="p-3 w-full bg-gray-100 border-b border-gray-300">
+		<div className="flex flex-row justify-between space-x-96 ">
 			<div className="float-left">
-				<Link href="/">
-					<img className="h-8 w-8 float-left" src="/logo.png" alt="" />
-				</Link>
+				<div className="font-normal text-xl space-y-2 w-full mb-2">
+					<Link href="/">
+						<img className="h-8 w-8 float-left" src="/logo.png" alt="" />
+					</Link>
+					<span className="mx-2">ZenTeach</span>
+				</div>
 			</div>
+			<div className="w-48 h-full"></div>
 			<div className="float-right">
-				<Link href="/features">
-					<a className="ml-8 font-medium text-gray-900">Features</a>
-				</Link>
-				<Link href="/contact">
-					<a className="ml-8 font-medium text-gray-900">Contact</a>
-				</Link>
-				<Link href="/portal">
-					<button className="inline-block ml-8 bg-red-400 text-white p-2 rounded font-small text-sm overflow-hidden">Go to Portal</button>
+				<Link href="https://portal.zenteach.co.uk">
+					<a
+						className="inline-block ml-8 bg-red-400 text-white p-2 rounded font-small text-md overflow-hidden"
+						href="https://portal.zenteach.co.uk"
+						target="_blank">
+					Go to Portal</a>
 				</Link>
 			</div>
 		</div>
