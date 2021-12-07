a0158c5dccd24b52178979d524c8b3bea40ff75c
Natnael Getahun <connect@ngetahun.me>
2021-12-07 20:05:45 +0100

    Add GA tag in site layout
    Send next.js metrics to GA

--- components/SiteLayout.js
+++ components/SiteLayout.js
@@ -6,17 +6,34 @@ import Footer from './Footer'
 
 export default class SiteLayout extends React.Component {
 	render() {
+		const GA_TRACKING_ID = process.env.GA_ANALYTICS;
+		const gaScript = `https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`
 		return (
 			<div className="bg-white lg:antialiased">
-					<Head>
-						<title>{this.props.title}</title>
-						<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-					</Head>
-					<div className="max-w-full sm:max-w-full mx-auto">
-						<NavBar />
-					</div>
+				<Head>
+					<title>{this.props.title}</title>
+					<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+					<script async src={gaScript}></script>
+					<script
+						async
+						dangerouslySetInnerHTML={{
+							__html: `
+									window.dataLayer = window.dataLayer || []
+									function gtag(){
+										dataLayer.push(arguments)
+									}
+									gtag('js', new Date())
+
+									gtag('config', ${GA_TRACKING_ID}, {
+										page_path: window.location.pathname,
+									  })`
+						}} />
+				</Head>
+				<div className="max-w-full sm:max-w-full mx-auto">
+					<NavBar />
+				</div>
 				<div>{this.props.children}</div>
-					<Footer />
+				<Footer />
 			</div>
 		);
 	}
