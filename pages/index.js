ff6e3ae1f7a10dff916ccfa855d1ea94b7d9bbd5
Natnael Getahun <connect@ngetahun.me>
2022-01-11 20:27:32 +0100

    Add caching

--- pages/index.js
+++ pages/index.js
@@ -289,3 +289,16 @@ class Index extends React.Component {
 }
 
 export default Index
+
+export async function getServerSideProps({ req, res }) {
+	res.setHeader(
+	  'Cache-Control',
+	  'public, s-maxage=10, stale-while-revalidate=59'
+	)
+
+	return {
+	  props: {
+		time: new Date().toISOString(),
+	  },
+	}
+  }
